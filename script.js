6c4c1f7388da7614ff88f6e5ed1d3548b474da66
steniowrsilva <steniowoney@gmail.com>
2023-06-27 23:31:53 -0300

    add code to displayBook function and add style to the table that contains the books informations

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const books = document.querySelector('.books');
 
 function Book(title, author, numberOfPages) {
   this.title = title;
@@ -14,4 +15,18 @@ function addBookToLibrary(title, author, numberOfPages) {
 
 function displayBooks() {
   // iterate through the array to display the books on the page
+  for(let i = 0; i<myLibrary.length; i++){
+    const bookShelf = document.createElement('tr');
+    bookShelf.innerHTML = `<td>${myLibrary[i].title}</td> <td>${myLibrary[i].author}</td> <td>${myLibrary[i].numberOfPages}</td>`;
+
+    books.appendChild(bookShelf);
+
+  }
 }
+// -------------------------------------------------------------------------------
+
+addBookToLibrary('auto da compadecida', 'suassuna', 200);
+
+addBookToLibrary('memórias póstumas', 'machado', 200);
+
+displayBooks();
